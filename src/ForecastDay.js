46d4818151093741e4119db5316867fd92f2274e
Polina <polinastuff@gmail.com>
2024-01-21 17:17:39 +0200

    fixed component

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,6 +1,6 @@
 import React from "react"; 
 
-export default function({timestamp}){
+export default function ForecastDay ({timestamp}){
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
