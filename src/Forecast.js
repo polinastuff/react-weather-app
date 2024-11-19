a4cd3663363e5d221e787cf6df743eebb2c04d7e
Polina <polinastuff@gmail.com>
2024-11-20 01:16:45 +0200

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -38,7 +38,7 @@ if (loaded) {
         </div>
     )
 } else {
-  let apiKey="8402ccd9e55983fce71eeeaa1d2bd1fc";
+  let apiKey="cf6b50b908fa2e0baca3eed8a569a5f6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&exclude=hourly,minutely&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
